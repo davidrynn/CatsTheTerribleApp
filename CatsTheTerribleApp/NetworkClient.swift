29556df4b7b5fedc127ba98d9c78822e0b99f6a3
davidrynn <drynn@doitt.nyc.gov>
2018-10-22 18:06:27 -0400

    Initial commit, added networking client class

---
+++ CatsTheTerribleApp/NetworkClient.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  CatsTheTerribleApp
+//
+//  Created by Rynn, David on 10/22/18.
+//  Copyright © 2018 Rynn, David. All rights reserved.
+//
+
+import Foundation
---
+++ MediaFile.swift
@@ -0,0 +1,9 @@
+//
+//  MediaFile.swift
+//  CatsTheTerribleApp
+//
+//  Created by Rynn, David on 10/22/18.
+//  Copyright © 2018 Rynn, David. All rights reserved.
+//
+
+import Foundation
